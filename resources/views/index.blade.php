1ae31101ceab76eba807820bc4795120bc1000ce
chin <149858139+dangkhoa2004@users.noreply.github.com>
2025-03-12 15:53:27 +0700

    [UI] UPDATE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Đây là trang web bán nhà đất khu vực Hải Phòng">
-    <title>Nhà Đất Hải Phòng</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <meta property="og:title" content="Nhà đất Phạm Dương" />
     <meta property="og:description" content="Chuyên buôn bán, đầu tư, môi giới nhà đất tại Hải Phòng" />
     <meta property="og:image" content="https://example.com/path/to/your-thumbnail-image.jpg" />
     <meta property="og:url" content="https://example.com" />
+    <title>Nhà Đất Hải Phòng</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         html,
         body,
