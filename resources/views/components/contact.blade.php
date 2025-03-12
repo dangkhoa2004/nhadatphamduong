6876f763ec9fad738d7f05f88985559267a661e7
chin <149858139+dangkhoa2004@users.noreply.github.com>
2025-03-12 15:19:15 +0700

    [UI & PER] UPDATE COMPONENTS & VIEW & CONTROLLER

--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed bottom-10 right-5 flex flex-col space-y-3 z-50">
     <a href="https://zalo.me/0389616946" target="_blank" class="w-12 h-12 bg-blue-500 text-white flex items-center justify-center rounded-full shadow-lg">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Icon_of_Zalo.svg" alt="Zalo" class="w-6 h-6">
+        <img src="http://phamduong1983.com/storage/images/Zalo.webp" alt="Zalo" class="w-6 h-6">
     </a>
     <a href="https://www.facebook.com/duongg.heo" target="_blank" class="w-12 h-12 bg-blue-700 text-white flex items-center justify-center rounded-full shadow-lg">
         <i class="fab fa-facebook-f"></i>
