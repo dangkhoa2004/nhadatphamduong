b0c6de39614dd155e7cc8a5a91bf4c510f646734
chin <149858139+dangkhoa2004@users.noreply.github.com>
2025-03-19 15:12:59 +0700

    [UI & PER] UPDATE COMPONENTS & VIEW & VALIDATION

--- resources/views/layouts/partials/banner-page.blade.php
+++ resources/views/layouts/partials/banner-page.blade.php
@@ -1,4 +1,3 @@
-<div class="flex justify-center">
-    <!-- <img src="{{ asset('storage/images/2366.gif' ) }}" alt="Phạm Dương" loading="lazy" style="width: 1170px; height: 140px;"> -->
-    <img src="" alt="Phạm Dương" loading="lazy" style="width: 1170px; height: 140px;">
+<div class="flex max-w-screen-xl mx-auto">
+    <img src="{{ asset('storage/images/banner.png' ) }}" alt="Phạm Dương" loading="lazy" style="width: 100%; height: 100%;">
 </div>
